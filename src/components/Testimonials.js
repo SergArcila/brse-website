06576ec3546dab4f9e8aab37a7ad10ba6ca08103
Sergio Arcila <SergioArcila@ufl.edu>
2024-01-24 19:15:50 -0500

    Fixing home page

--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { FaStar } from 'react-icons/fa'; // Ensure you have installed react-icons
 
 function TestimonialsSection() {
   return (
@@ -10,33 +11,49 @@ function TestimonialsSection() {
         </div>
         <div className="flex flex-wrap -m-4">
           {/* Testimonial 1 */}
+          {/* ... other testimonials ... */}
           <div className="p-4 md:w-1/2 lg:w-1/3">
-            <div className="h-full bg-white bg-opacity-90 px-8 pt-8 pb-12 rounded-lg overflow-hidden text-center relative">
-              <h2 className="tracking-widest text-xs title-font font-medium text-gray-500 mb-1">THE REALTOR</h2>
-              <h1 className="title-font sm:text-2xl text-xl font-medium text-gray-900 mb-3">Becky Robinson</h1>
+            <div className="h-full bg-white bg-opacity-90 p-8 rounded-lg overflow-hidden text-center relative">
               <p className="leading-relaxed mb-3">"They are very professional. They show up on time to every shoot and deliver quality photos and video to each without fail."</p>
+              <div className="flex justify-center mb-4">
+                {[...Array(5)].map((_, i) => (
+                  <FaStar key={i} className="text-yellow-500" />
+                ))}
+              </div>
+              <h2 className="tracking-widest text-xs title-font font-medium text-gray-500">THE REALTOR</h2>
+              <h1 className="title-font sm:text-2xl text-xl font-medium text-gray-900">Becky Robinson</h1>
               {/* Realtor's logo here */}
             </div>
           </div>
-          {/* Testimonial 2 */}
           <div className="p-4 md:w-1/2 lg:w-1/3">
-            <div className="h-full bg-white bg-opacity-90 px-8 pt-8 pb-12 rounded-lg overflow-hidden text-center relative">
-              <h2 className="tracking-widest text-xs title-font font-medium text-gray-500 mb-1">THE AGENT</h2>
-              <h1 className="title-font sm:text-2xl text-xl font-medium text-gray-900 mb-3">Jeremy Bravo</h1>
+            <div className="h-full bg-white bg-opacity-90 p-8 rounded-lg overflow-hidden text-center relative">
               <p className="leading-relaxed mb-3">"Their communication and service is among the best in the industry."</p>
-              {/* Agent's logo here */}
+              <div className="flex justify-center mb-4">
+                {[...Array(5)].map((_, i) => (
+                  <FaStar key={i} className="text-yellow-500" />
+                ))}
+              </div>
+              <h2 className="tracking-widest text-xs title-font font-medium text-gray-500">THE AGENT</h2>
+              <h1 className="title-font sm:text-2xl text-xl font-medium text-gray-900">Jeremy Bravo</h1>
+              {/* Realtor's logo here */}
             </div>
           </div>
-          {/* Testimonial 3 */}
           <div className="p-4 md:w-1/2 lg:w-1/3">
-            <div className="h-full bg-white bg-opacity-90 px-8 pt-8 pb-12 rounded-lg overflow-hidden text-center relative">
-              <h2 className="tracking-widest text-xs title-font font-medium text-gray-500 mb-1">THE BROKER</h2>
-              <h1 className="title-font sm:text-2xl text-xl font-medium text-gray-900 mb-3">Zeke Dorr</h1>
+            <div className="h-full bg-white bg-opacity-90 p-8 rounded-lg overflow-hidden text-center relative">
               <p className="leading-relaxed mb-3">"I have incredibly high expectations and BRSE Real Estate Photography exceeds them every time."</p>
-              {/* Broker's logo here */}
+              <div className="flex justify-center mb-4">
+                {[...Array(5)].map((_, i) => (
+                  <FaStar key={i} className="text-yellow-500" />
+                ))}
+              </div>
+              <h2 className="tracking-widest text-xs title-font font-medium text-gray-500">THE BROKER</h2>
+              <h1 className="title-font sm:text-2xl text-xl font-medium text-gray-900">Zeke Dorr</h1>
+              {/* Realtor's logo here */}
             </div>
           </div>
-          {/* More testimonials */}
+          
+
+          {/* ... other testimonials ... */}
         </div>
       </div>
     </section>
--- src/components/Valuesection.js
+++ src/components/Valuesection.js
@@ -6,45 +6,55 @@ function ValuesSection() {
     <section className="text-gray-600 body-font">
       <div className="container px-5 py-16 mx-auto">
         <div className="flex flex-col text-center w-full mb-20">
-        <img src={"/100.png"} alt="Elevating Listings" className="mx-auto mb-4" style={{ maxHeight: '70px' }} /> {/* Adjust maxHeight as needed */}
-          <h2 className="text-4xl font-medium title-font mb-4 text-gray-900">We Exist To Serve You, The Agent</h2>
+      
+        <h2 className="text-gray-900 text-6xl mb-4 font-bold title-font pb-3">
+  We Exist To Serve You, <span className="text-orange-500">The Agent</span>
+</h2>
           <p className="lg:w-2/3 mx-auto leading-relaxed text-base">At BRSE Real Estate Photography, we're not just a company; we're your creative partners in real estate marketing. We specialize in crafting striking visuals through professional property photography and videography, enhancing your brand identity, and supporting your sales efforts. Our skilled team, comprising over two decades of collective experience with photographers, visual editors, and client relations specialists, is at the forefront of delivering bespoke real estate media solutions. Your dedication to your brand resonates with us, and we're here to ensure that every snapshot and frame adds value to your hard-earned reputation.
 </p>
         </div>
         <div className="flex flex-wrap">
           
           <div className="p-4 md:w-1/4 sm:w-1/2 w-full">
-            <div className="border-2 border-gray-200 px-4 py-6 rounded-lg">
-              <div className="w-12 h-12 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-4">
-                <i className="your-icon-class-for-time"></i> 
-              </div>
+            <div className=" px-4 py-6 rounded-lg">
+            <img
+        src="/time.png" // Replace with the path to your image
+        alt="On Time"
+        className="w-32 h-32  mx-auto mb-4"
+      />
               <h2 className="title-font font-medium text-3xl text-gray-900">We Show Up On Time</h2>
             </div>
           </div>
 
           <div className="p-4 md:w-1/4 sm:w-1/2 w-full">
-            <div className="border-2 border-gray-200 px-4 py-6 rounded-lg">
-              <div className="w-12 h-12 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-4">
-                <i className="your-icon-class-for-love"></i>
-              </div>
+          <div className=" px-4 py-6 rounded-lg">
+            <img
+        src="/love.png" // Replace with the path to your image
+        alt="On Time"
+        className="w-32 h-32  mx-auto mb-4"
+      />
               <h2 className="title-font font-medium text-3xl text-gray-900">We Love Our Clients</h2>
             </div>
           </div>
 
           <div className="p-4 md:w-1/4 sm:w-1/2 w-full">
-            <div className="border-2 border-gray-200 px-4 py-6 rounded-lg">
-              <div className="w-12 h-12 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-4">
-                <i className="your-icon-class-for-efficiency"></i>
-              </div>
+          <div className=" px-4 py-6 rounded-lg">
+            <img
+        src="/efficient.png" // Replace with the path to your image
+        alt="On Time"
+        className="w-32 h-32  mx-auto mb-4"
+      />
               <h2 className="title-font font-medium text-3xl text-gray-900">We Are Efficient & Friendly</h2>
             </div>
           </div>
 
           <div className="p-4 md:w-1/4 sm:w-1/2 w-full">
-            <div className="border-2 border-gray-200 px-4 py-6 rounded-lg">
-              <div className="w-12 h-12 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-4">
-                <i className="your-icon-class-for-pricing"></i>
-              </div>
+          <div className=" px-4 py-6 rounded-lg">
+            <img
+        src="/pricing.png" // Replace with the path to your image
+        alt="On Time"
+        className="w-32 h-32  mx-auto mb-4"
+      />
               <h2 className="title-font font-medium text-3xl text-gray-900">We Have Simple Pricing</h2>
             </div>
           </div>
